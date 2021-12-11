96450fd744ceb1134c08872a678d05a1e6085bba
David1Savitsky <omgprogress24@gmail.com>
2021-12-11 20:27:57 +0300

    com

--- src/main/java/com/epam/cube/CubeObservable.java
+++ src/main/java/com/epam/cube/CubeObservable.java
@@ -2,6 +2,7 @@ package com.epam.cube;
 
 import com.epam.cube.entity.Cube;
 import com.epam.cube.entity.Point;
+import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,8 @@ public class CubeObservable extends Cube implements Observable {
 
     private final Integer id;
     private final List<Observer> observers = new ArrayList<>();
+
+    public static final Logger LOGGER = Logger.getLogger(CubeObservable.class);
 
     public CubeObservable(Integer id, Point startingPoint, double side) {
         super(startingPoint, side);
@@ -47,6 +50,7 @@ public class CubeObservable extends Cube implements Observable {
         for(Observer observer : observers){
             observer.update(this);
         }
+        LOGGER.info("All observers have been notified");
     }
 
 
